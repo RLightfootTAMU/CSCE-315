3d980149435c28997ab16c68fb5f0c63ff4cdd4c
RLightfootTAMU <rosslightfoot@tamu.edu>
2016-02-01 01:21:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,38 +7,36 @@ using namespace std;
 
 //structs
 struct Achievement{
-    string gameID;
+    int gameID;
     int achievementID;
     string achievementName;
     int achievementPoints;
+    int timesAchieved;
+    vector<int> PlayersWithAchievement;
 };
 struct Game{
     int gameID;
     string gameName;
-    vector<Achievement> achievementVector;
-};
-struct GamePlayed{
-    int gameID;
-    vector<Achievement> achievementVector;
-    string gameName;
     string IGN;
-    int achievementCount;
-    int maxpoints;
-    int currentpoints;
+    int currentScore;
+    vector<Achievement> achievementVector;
+    vector<int> peopleThatPlay;
 };
 struct Player{
     int playerID;
     string playerName;
     int gamerPoints;
     vector<Achievement> achievements;
+    vector<Game> inGameNames;
+    vector<Game> gameScores;
     vector<int> friendList;
-    vector<GamePlayed> gameList;
+    vector<int> gameList;
 };
 
 //function prototypes
 int getCommand();
-void AddPlayer(int playerId, string playerName);
-void AddGame(int gameId, string gameName);
+void AddPlayer(int, string);
+void AddGame(int, string);
 void AddAchievement(int, int, string, int);
 void Plays(int, int, string);
 void AddFriends(int, int);
@@ -49,21 +47,14 @@ void SummarizePlayer(int);
 void SummarizeGame(int);
 void SummarizeAchievement(int, int);
 void AchievementRanking();
-void printGameDB();
-void printPlayerDB();
-
-
-
-//Player* findPlayer(int);
+//extra functions to help my program
 vector<Player>::iterator findPlayer(int);
 vector<Game>::iterator findGame(int);
-vector<Achievement>::iterator findAchievement(int);
-Player* globalPtr;
+vector<Achievement>::iterator findAchievement(int, int);
 
 //global data structures
 vector<Game> gameDB;        //global game database
 vector<Player> playerDB;    //global player database
-vector<Achievement> achievementDB;
 
 int main(){
     int temp = 1;
@@ -71,9 +62,6 @@ int main(){
     while(temp != 0){
         temp = getCommand();
     }
-    //cout << "Please enter a command\n";
-    //cin >> tempCommand;
-    
 }
 
 int getCommand(){
@@ -94,13 +82,11 @@ int getCommand(){
         cin >> tempInt1;
         getline(cin, tempS1, '"');
         getline(cin, tempS2, '"');
-        //cin >> tempS1;
         AddPlayer(tempInt1, tempS2);
         return 1;
     }
     else if(tempCommand == "AddGame"){
         cin >> tempInt1;
-        //cin >> tempS1;
         getline(cin, tempS1, '"');
         getline(cin, tempS2, '"');
         AddGame(tempInt1, tempS2);
@@ -111,7 +97,6 @@ int getCommand(){
         cin >> tempInt2;
         getline(cin, tempS1, '"');
         getline(cin, tempS2, '"');
-        //cin >> tempS3;
         cin >> tempInt3;
         AddAchievement(tempInt1, tempInt2, tempS2, tempInt3);
         return 1;
@@ -119,16 +104,14 @@ int getCommand(){
     else if(tempCommand == "Plays"){
         cin >> tempInt1;
         cin >> tempInt2;
-        //getline(cin, tempS1, '"');
         getline(cin, tempS1);
-        //cin >> tempS2;
         Plays(tempInt1, tempInt2, tempS1);
         return 1;
     }
     else if(tempCommand == "AddFriends"){
         cin >> tempInt1;
         cin >> tempInt2;
-        //AddFriends(tempInt1, tempInt2);
+        AddFriends(tempInt1, tempInt2);
         return 1;
     }
     else if(tempCommand == "Achieve"){
@@ -167,18 +150,8 @@ int getCommand(){
         SummarizeAchievement(tempInt1, tempInt2);
         return 1;
     }
-    else if(tempCommand == "PrintGames"){
-        printGameDB();
-        return 1;
-    }
-    else if(tempCommand == "PrintPlayers"){
-        printPlayerDB();
-        return 1;
-    }
-    else if(tempCommand == "Find"){
-        cin >> tempInt1;
-        //Player* pptr = findPlayer(tempInt1);
-        findPlayer(tempInt1);
+    else if(tempCommand == "AchievementRanking"){
+        AchievementRanking();
         return 1;
     }
     else {
@@ -192,7 +165,6 @@ void AddPlayer(int pID, string pName){
     p1.playerID = pID;
     p1.playerName = pName;
     p1.gamerPoints = 0;
-    //cout << "The Player name is " << p1.playerName << "\n";
     playerDB.push_back(p1);
 }
 
@@ -204,57 +176,81 @@ void AddGame(int gID, string gName){
 }
 
 void AddAchievement(int gID, int aID, string aName, int aPoints){
+    //create achievement object from given vals and push into the respective game's achievement vector 
     Achievement a1;
     a1.gameID = gID;
     a1.achievementID = aID;
     a1.achievementName = aName;
     a1.achievementPoints = aPoints;
-    cout << "Game id: " << gID << "\n";
-    vector<Game>::iterator it;
-    it = findGame(gID);
-    it->achievementVector.push_back(a1);
-    achievementDB.push_back(a1);
-    //cout << "A name is: " << aName << "\n";
-    //cout << "A points is: " << aPoints << "\n";
+    a1.timesAchieved = 0;
+    vector<Game>::iterator git;
+    git = findGame(gID);
+    git->achievementVector.push_back(a1);
 }
 
 void Plays(int pID, int gID, string iGN){
-    vector<Player>::iterator it;
-    it = findPlayer(pID);
-    GamePlayed gp1;
-    gp1.IGN = iGN;
-    gp1.achievementCount = 0;
-    gp1.gameID = gID;
-    it->gameList.push_back(gp1);
-    //search player db for pID, pushback players game vector
+    //search player db for pID & gID, pushback players record vectors
+    vector<Player>::iterator pit;
+    vector<Game>::iterator git;
+    pit = findPlayer(pID);
+    git = findGame(gID);
+    Game g1;
+    g1.IGN = iGN;
+    g1.gameID = gID;
+    Game g2;
+    g2.gameID = gID;
+    g2.currentScore = 0;
+    pit->gameList.push_back(gID);
+    pit->inGameNames.push_back(g1);
+    pit->gameScores.push_back(g2);
+    vector<int>::iterator it = git->peopleThatPlay.begin();
+    while(it != git->peopleThatPlay.end()){
+        if(pID == *it){
+            cout << "Holllllleee up, mannnnnnnnnnnnn\n";
+            break;
+        }
+        it++;
+    }
+    git->peopleThatPlay.push_back(pID);
 }
 
 void AddFriends(int pID1, int pID2){
+    //search player db for pIDs, pushback players respective friend vectors with the other players pID
     vector<Player>::iterator it1, it2;
     it1 = findPlayer(pID1);
     it2 = findPlayer(pID2);
     it1->friendList.push_back(pID2);
     it2->friendList.push_back(pID1);
-    //search player  db for pIDs, add other pid to friend list
 }
 
 void Achieve(int pID, int gID, int aID){
+    //search player db for pID, create achievement object and update the players record by pushing the achievement vector and updating points
     vector<Player>::iterator playerit;
     vector<Achievement>::iterator ait;
-    ait = findAchievement(aID);
+    ait = findAchievement(aID, gID);
     Achievement a1 = *ait;
+    playerit = findPlayer(pID);
     playerit->achievements.push_back(a1);
     playerit->gamerPoints = (playerit->gamerPoints) + (ait->achievementPoints);
-    //search playerdb for player, add achievement to the achievement vector
+    ait->PlayersWithAchievement.push_back(pID);
+    ait->timesAchieved = (ait->timesAchieved)+1;
+    vector<Game>::iterator git = playerit->gameScores.begin();
+    while(git != playerit->gameScores.end()){
+        if(git->gameID == gID)
+           git->currentScore = (git->currentScore)+(ait->achievementPoints); 
+        git++;
+    }
 }
 
 void FriendsWhoPlay(int pID, int gID){
-   // Player* ptr = findPlayer(pID);
-   /* vector<Player>::iterator p1;
+    //search player db for pIDs, cross-reference for the other pID and print when found
+    vector<Player>::iterator p1;
     p1 = findPlayer(pID);
     vector<int>::iterator it1;
     vector<int>::iterator it2;
     vector<Player>::iterator p2;
+    vector<Game>::iterator git = findGame(gID);
+    cout << "\nGame Name: " << git->gameName << "\n";
     it1 = p1->friendList.begin();
     while(it1 != p1->friendList.end()){
         int temporary = *it1;
@@ -262,110 +258,195 @@ void FriendsWhoPlay(int pID, int gID){
         it2 = p2->gameList.begin();
         while(it2 != p2->gameList.end()){
             if(*it2 == gID)
-                cout << "Player ID: " << p2->playerID << " plays the game\n";
+                cout << "Player ID: " << p2->playerID << "\tPlayer name: " << p2->playerName << " plays the game\n";
             it2++;
         }
         it1++;
-    }*/
-   
-    //get players friendlist & cross-references for the specified gameID
+    }
+    cout << "\n";
 }
 
 void ComparePlayers(int pID1, int pID2, int gID){
-    //compare players records and scores for the given game
+    //find player1 and iterate through their records in order to print the necessary info, then do it again for player 2
+    vector<Player>::iterator p1 = findPlayer(pID1);
+    vector<Player>::iterator p2 = findPlayer(pID2);
+    vector<Game>::iterator g1 = findGame(gID);
+    cout << "\nGame name: " << g1->gameName << "\n";
+    cout << "------------------\nPlayer 1 Record:\n";
+    cout << "Gamerscore: ";
+    g1 = p1->gameScores.begin();
+    while(g1 != p1->gameScores.end()){
+        if(g1->gameID == gID)
+            cout << g1->currentScore;
+        g1++;
+    }
+    cout << "\n------------------\nAchievements: \n";
+    vector<Achievement>::iterator a1 = p1->achievements.begin();
+    while(a1 != p1->achievements.end()){
+        if(a1->gameID == gID)
+            cout << a1->achievementName << "\n";
+        a1++;
+    }
+    cout << "\nPlayer 2 Record:\n";
+    cout << "Gamerscore: ";
+    g1 = p2->gameScores.begin();
+    while(g1 != p2->gameScores.end()){
+        if(g1->gameID == gID)
+            cout << g1->currentScore;
+        g1++;
+    }
+    cout << "\n------------------\nAchievements: \n";
+    a1 = p2->achievements.begin();
+    while(a1 != p2->achievements.end()){
+        if(a1->gameID == gID)
+            cout << a1->achievementName << "\n";
+        a1++;
+    }
+    cout << "\n";
 }
 
 void SummarizePlayer(int pID){
-    //report the players friends, games they play, and point totals
-    vector<Player>::iterator it;
-    it = findPlayer(pID);
-    cout << "Player: " << it->playerName << "\n";
-    cout << "Total Gamerscore: " << it->gamerPoints << "\n";
-    cout << "Game \t Achievements \t Gamerscore \t IGN\n";
-    vector<GamePlayed>::iterator git;
-    git = it->gameList.begin();
-    while(git != gameList.end()){
-        cout << git->gameName << "\t";
-        
+    //Find the player, then use iterators to print the neccessary information from their record
+    vector<Player>::iterator pit;
+    vector<Player>::iterator ptemp;
+    vector<Game>::iterator temp;
+    vector<Game>::iterator igns;
+    pit = findPlayer(pID);
+    cout << "Player: " << pit->playerName << "\n------------------\n";
+    cout << "Total Gamerscore: " << pit->gamerPoints << "\n";
+    vector<Game>::iterator git = pit->gameScores.begin();
+    while(git != pit->gameScores.end()){
+        temp = findGame(git->gameID);
+        cout << "Game: " << temp->gameName << "\tpoints: " << git->currentScore << "\tin-game name: ";
+        igns = pit->inGameNames.begin();
+        while(igns != pit->inGameNames.end()){
+            if(igns->gameID == temp->gameID)
+                cout << igns->IGN;
+            igns++;
+        }
+        cout << "\n";
         git++;
     }
+    cout << "------------------\nFriends: \n";
+    vector<int>::iterator friendit = pit->friendList.begin();
+    while(friendit != pit->friendList.end()){
+        ptemp = findPlayer(*friendit);
+        cout << "Name: " << ptemp->playerName << "\tGamerscore: " << ptemp->gamerPoints << "\n";
+        friendit++;
+    }
+    cout << "\n";
 }
 
 void SummarizeGame(int gID){
-    //report nummber of players playing the specified game and the achievement totals
+    //use the game database to locate the game. Use the achievement vector and the people who play vectors to print the players and achievements for the specified game
+    vector<Game>::iterator git = findGame(gID);
+    vector<Player>::iterator pit;
+    vector<Achievement>::iterator ait;
+    vector<int>::iterator it = git->peopleThatPlay.begin();
+    cout << "\nGame Name: " << git->gameName << "\n------------------\n";
+    cout << "Players: \n";
+    while(it != git->peopleThatPlay.end()){
+        pit = findPlayer(*it);
+        cout << pit->playerName << " played the game\n";
+        it++;
+    }
+    cout <<  "------------------\n";
+    ait = git->achievementVector.begin();
+    while(ait != git->achievementVector.end()){
+        cout << "Achievement: " << ait->achievementName << " completed " << ait->timesAchieved << " times\n";
+        ait++;
+    }
+    cout << "\n";
 }
 
 void SummarizeAchievement(int gID, int aID){
-    
+    //use the gameDB vector along with counter variales to count the players both playing the game and who have achieved the specified achievement as they are printed
+    int counter1 = 0;
+    int counter2 = 0;
+    double percentage;
+    vector<Achievement>::iterator it;
+    vector<Player>::iterator pit;
+    vector<Game>::iterator git = findGame(gID);
+    it = findAchievement(aID, gID);
+    cout << "\nAchievement Name: " << it->achievementName << "\n------------------\n" << "Players who have the Achievement: \n";
+    vector<int>::iterator it2 = it->PlayersWithAchievement.begin();
+    while (it2 != it->PlayersWithAchievement.end()){
+        counter1++;
+        cout << counter1 << " ";
+        pit = findPlayer(*it2);
+        cout << pit->playerName << "\n";
+        it2++;
+    }
+    cout << "------------------\npercentage of players who played with achievement: ";
+    vector<int>::iterator it3 = git->peopleThatPlay.begin();
+    while (it3 != git->peopleThatPlay.end()){
+        counter2++;
+        it3++;
+    }
+    percentage = (counter1*100)/counter2;
+    cout << percentage << " percent\n\n";
 }
 
-//Player* findPlayer(int pID){
+void AchievementRanking(){
+    //Copy the playerDB vector and delete the highest entry as it is printed until the copied vector is empty
+    cout << "\n";
+    vector<Player>::iterator current;
+    vector<Player>::iterator high;
+    int temp, temphigh, index;
+    index = 0;
+    vector<Player> playerDBcopy = playerDB; 
+    while(playerDBcopy.begin() != playerDBcopy.end()){
+        index++;
+        current = playerDBcopy.begin();
+        high = playerDBcopy.begin();
+        current++;
+        while(current != playerDBcopy.end()){
+            if(current->gamerPoints > high->gamerPoints)
+                high = current;
+            current++;
+        }
+        cout << index << " Name: " << high->playerName << "\tGamerpoints: " << high->gamerPoints << "\n";
+        playerDBcopy.erase(playerDBcopy.begin());
+    }
+    cout << "\n";
+}
+
 vector<Player>::iterator findPlayer(int pID){
+    //extra function to locate a player in the playerDB given pID
     vector<Player>::iterator it;
     it = playerDB.begin();
     while(it != playerDB.end()){
         if(it->playerID == pID){
-            Player* found;
-            //found = *it;
-            //*found = *it;
-            //*globalPtr = *it;
-            cout << "Player was found\n";
             return it;
-            //return globalPtr;
         }
         it++;
     }
     cout << "player not found \n";
-    //return NULL;
 }
 
 vector<Game>::iterator findGame(int gID){
+    //extra function to locate a game in the gameDB given gID
     vector<Game>::iterator it;
     it = gameDB.begin();
     while(it != gameDB.end()){
         if(it->gameID == gID){
-            //Game *found;
-            //*found = *it;
             return it;
         }
         ++it;
     }
     cout << "game not found \n";
-    //return NULL;
 }
 
-vector<Achievement>::iterator findAchievement(int aID){
+vector<Achievement>::iterator findAchievement(int aID, int gID){
+    //extra function to locate an achievement in the gameDB given both the aID and gID
     vector<Achievement>::iterator it;
-    it = achievementDB.begin();
-    while(it != achievementDB.end()){
+    vector<Game>::iterator git = findGame(gID);
+    it = git->achievementVector.begin();
+    while(it != git->achievementVector.end()){
         if(it->achievementID == aID){
-            //Achievement *found;
-            //*found = *it;
             return it;
         }
         ++it;
     }
     cout << "achievement not found \n";
-}
-
-void printGameDB(){
-    vector<Game>::iterator it;
-    it = gameDB.begin();
-    while(it != gameDB.end()){
-        cout << "Name: " << it->gameName << "--------" << " Game id: " << it->gameID << "\n";
-        ++it;
-    }
-    //cout << "Name: " << it->gameName << "--------" << " Game id: " << it->gameID << "\n";
-    return;
-}
-
-void printPlayerDB(){
-    vector<Player>::iterator it;
-    it = playerDB.begin();
-    while(it != playerDB.end()){
-        cout << "Name: " << it->playerName << "--------" << " Player id: " << it->playerID << "\n";
-        ++it;
-    }
-    //cout << "Name: " << it->gameName << "--------" << " Game id: " << it->gameID << "\n";
-    return;
 }
